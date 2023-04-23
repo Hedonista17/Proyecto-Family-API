a83f30c65e50cfdf30a292cdd06a978a9ea83585
Hedonista17 <andreaynereapiza@gmail.com>
2023-04-23 15:17:20 +0000

    Realizado Ejercicio CRUD

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
+from datastructures import FamilyStructure  # De esta forma importamos las funcionalidades para generar una id random o el apellido que siempre es el mismo 
 #from models import Person
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ app.url_map.strict_slashes = False
 CORS(app)
 
 # create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+jackson_family = FamilyStructure("Jackson")   # Añadimos esta var como dato-estructura fija del apellido 
 
 member1 = {
     "id": jackson_family._generateId(),
@@ -22,8 +22,25 @@ member1 = {
     "age":33,
     "lucky_numbers":[7,13,22]
 }
-jackson_family.add_member(member1)
 
+member2 = {
+    "id": jackson_family._generateId(),
+    "first_name":"Jane",
+    "last_name":jackson_family.last_name,
+    "age":35,
+    "lucky_numbers":[10,14,3]
+}
+
+member3 = {
+    "id": jackson_family._generateId(),
+    "first_name":"Jimmy",
+    "last_name":jackson_family.last_name,
+    "age":5,
+    "lucky_numbers":[1]
+}
+jackson_family.add_member(member1)  
+jackson_family.add_member(member2)
+jackson_family.add_member(member3)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -36,30 +53,41 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+def get_all_members():
+    members = jackson_family.get_all_members()  # del archivo de datastructures - funcion 
+    response_body = members
+    if members == None:
+        return jsonify({"msg": "Error al cargar la informacion","error":True}), 400
 
     return jsonify(response_body), 200
 
 
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member_by_id(member_id):
+    response_body = jackson_family.get_member(member_id) # del archivo de datastructures - funcion 
+    if response_body == None:
+        return jsonify({"msg":"Error al cargar la informacion","error":True}), 400
 
-@app.route('/members', methods=['POST'])
-def handle_create_member():
-        data = request.get_json()
-        new_member = jackson_family.add_member
+    return jsonify(response_body), 200
 
-        response_body = {
-        "hello": "world",
-        "family": new_member
-        }
-        return jsonify(response_body), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    response_body = {"delete_member":jackson_family.delete_member(member_id)} # del archivo de datastructures - funcion 
+    if response_body == None:
+        return jsonify({"msg":"Error al borrar miembro de la familia Jackson!","error":True}), 400
+
+    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.json()
+    if request_body == None:
+        return jsonify({"msg":"Error al añadir miembro a la familia Jackson!","error":True}), 400
+    jackson_family.add_member(request_body)  # del archivo de datastructures - funcion 
+    response_body = ""
+    return jsonify(response_body), 200
+
 
 
 # this only runs if `$ python src/app.py` is executed
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,31 +11,35 @@ from random import randint
 class FamilyStructure:
     def __init__(self,last_name):
      
-        self._members = []
+        self._members = []                        # Los miembros de la familia sera lista de objetos 
         
-        self.last_name = last_name
+        self.last_name = last_name              
        
 
     # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
+    def _generateId(self):                          # Funcion que genera una id random a los id de la familia 
         return randint(0, 99999999)
 
     def add_member(self, member):
         # fill this method and update the return
-        member["last_name"] = self.last_name
-        if "id" not in member: member["id"] = self._generateId()
+        member["last_name"] = self.last_name       # Control del apellido solo el resto de campos se añadiran en app.py
+        if "id" not in member: member["id"] = self._generateId()    # Si el miembro no existe lo creas 
         self._members.append(member)
         return member
             
     
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+       for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return {"msg":"Miembro borrado correctamente!","error":False,"satus":200}
+            return {"msg":"Error al borrar miembro de la familia!","error":True,"satus":400}
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if memeber["id"] == id :
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
